34104152f454fb96f5d616d961f9d18868789f6b
Ricartho Franck <ricartho.franck@esih.edu>
2019-05-22 20:44:55 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,6 +4,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
    create_table :costume_stores do |col|
      col.string :name 
      col.string :location
+     col.integer :costume_inventory
      col.
    end 
   end 
